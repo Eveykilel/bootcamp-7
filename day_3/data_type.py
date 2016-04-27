512714215a799a1128dc3b58bc84828fb1c5beb1
Eveykilel <eveykilel@gmail.com>
2016-04-27 10:39:30 +0300

    Modied the changes to data_type

--- day_3/data_type.py
+++ day_3/data_type.py
@@ -13,7 +13,7 @@ def data_type(x):
 	elif type(x) == float:
 		return (x/2)
 	elif type(x) == str:
-		return ("hello" + x)
+		return ("Hello " + x)
 	elif type(x) == bool:
 		return ("boolean")
 	elif type(x) == long:
