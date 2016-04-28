e00299a19840d87d00e5f0337f7ae2c3f2018336
Eveykilel <eveykilel@gmail.com>
2016-04-28 17:12:13 +0300

    add packages

--- day_4/super_sum.py
+++ day_4/super_sum.py
@@ -1,8 +1,24 @@
-def super_sum():
+def super_sum(*args):
     '''
     Returns the sum of numbers.
     e.g 
         super_sum(1,2,3) ==> 6
+        super_sum("string") ==> 0
+        z = 10 + super_sum("bug")
         super_sum([1,2,3]) ==> 6
-        super_sum([10], []20,30) ==> 60
+        super_sum([10], [20], [30]) ==> 60
     '''
+    total = 0
+    if not args:
+        return 0
+    else:
+        for x in args:
+            if type(x) == list:
+                total += sum(x)
+            elif type(x) == str:
+                return 0
+            else:
+                total += x
+    return total
+
+    
--- day_4/test_super_sum.py
+++ day_4/test_super_sum.py
@@ -1,7 +1,24 @@
 #Test suite for super_sum
 from unittest import TestCase
+from super_sum import super_sum
 
 class  SuperSumTestCase(TestCase):
 	"""Test Case for super_sum."""
 
-		
+	#test empty input
+	def test_empty_input(self):
+		self.assertEqual(super_sum(), 0)
+
+		#Test sum of integers
+	def test_sum_of_integers(self):
+		self.assertEqual(super_sum(1, 2, 3), 6)
+		self.assertEqual(super_sum(-1, 1), 0)
+		self.assertNotEqual(super_sum(10, 20, 30), 100)
+
+		#Test sum of numbers in a lisrt
+	def test_sum_of_items_in_one_list(self):
+		self.assertEqual(super_sum([1, 2, 3]), 6)
+
+		#Tesy sum of a string
+	def test_string_input_returns(self):
+		self.assertEqual(super_sum('string'), 0)
