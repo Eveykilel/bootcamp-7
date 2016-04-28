57da6d787fea8ae696346ad856ef7ab169beaf4d
Eveykilel <eveykilel@gmail.com>
2016-04-28 18:15:12 +0300

    add new items

--- day_4/packagesexample/A/classes.py
+++ day_4/packagesexample/A/classes.py
@@ -2,9 +2,32 @@
 class Animal(object):
 	pass
 
-class Poacher(object):
-	pass
+class Person(object):
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def walk(self):
+		return "I'm walking"
+
+class Poacher(Person):
+
+	def __init__(self, name, age, **kwargs):
+		Person.__init__(self, name, age)
+		self.gun = kwargs.get('gun', 'AK-47')
+		self.loc = kwargs.get('loc', 'Nairobi')
+		self.game_park = kwargs.get('game_park', 'Tsavo')
+		self.fav = kwargs.get('fav', 'Elephant')
+
+		#for args
+		if args[0]
+
 
 class Tourist(object):
 	pass
-	
+
+p = Person('Jane', 23)
+pc = Poacher('Joe', 45, gun='Rifle', fav='Elephant', loc='Mombasa')
+
+print pc.name, pc.age, pc.guun
+
--- day_4/packagesexample/importer.py
+++ day_4/packagesexample/importer.py
@@ -1,5 +1,26 @@
 from A.classes import Animal, Poacher, Tourist
 from A.functions import word_count, sum_of_digits
 
-print Animal
-print word_count
+import A.functions as func
+import A.classes as classess
+
+print Animal, word_count
+print word_count, func.word_count
+
+# {
+# 	'Animal': <class>
+# 	'Poacher': <class>
+# 	'Tourist': <class>
+# 	'word_count':<func>
+# 	'sum_of_digits': <func>
+# 	'func':{
+# 		'word_count': <func>
+# 		'sum_of_digits': <func>
+# 	},
+# 	'classes':{
+# 		'Animal': <class>,
+# 		'Poacher': <class>,
+# 		'Tourist': <class>
+
+# 	}
+# }
